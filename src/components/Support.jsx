28fb3f412ee75eb85a88fd498bbd16d71b31606f
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-06-30 18:58:59 -0300

    hasta Support

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -5,7 +5,32 @@ import supportImg from '../assets/support.jpg'
 
 function Support() {
   return (
-    <div>Support</div>
+    <div className=''>
+        <div>
+            <img src={supportImg} alt="/" />
+        </div>
+        <div>
+            <div>
+                <h2>Support</h2>
+                <h3>Finding the right team</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque placeat fugit rem veritatis repudiandae quia, aliquam voluptate accusamus laudantium suscipit corrupti accusantium error architecto incidunt? Iure perferendis nostrum amet ducimus?</p>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <BsTelephone />
+                        <h3>Sales</h3>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In explicabo dolores minus, quia magnam nobis tenetur dolorum? Eos consectetur quas suscipit autem nulla doloribus dignissimos, eaque similique dolorem, adipisci ut.</p>
+                    </div>
+
+                    <div>
+                        <p>Contact Us <BsArrowRight/></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
